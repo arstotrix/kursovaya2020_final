13b71affa5565d1dd361b7ea3d5b31863523feed
Marie <mashamaslova85@gmail.com>
2020-10-09 16:50:23 +0300

    123456u

--- bert_analysis.py
+++ bert_analysis.py
@@ -1,18 +1,18 @@
 import os
-from bert_embedding import BertEmbedding
+#from bert_embedding import BertEmbedding
 
 curr_dir = os.getcwd()
 filepath = os.path.join(curr_dir, 'csv_files')
 files = os.listdir(filepath)
 #вот здесь ниже поменять циферку
-file =  os.path.join(filepath, files[0])
+file =  os.path.join(filepath, files[1])
 with open (file, encoding='utf-8') as f:
-    sentences = f.read()[1:].splitlines()
+    sentences = f.read().splitlines()
 print(sentences[0])
-bert_embedding = BertEmbedding()
-result = bert_embedding(sentences)
+#bert_embedding = BertEmbedding()
+#result = bert_embedding(sentences)
 
-print(result[0][1])
+#print(result[0][1])
 
 
 
